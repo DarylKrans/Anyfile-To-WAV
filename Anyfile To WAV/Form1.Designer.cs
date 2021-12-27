bcae1802ec5633a760d31fcd9979f3e848520182
Daryl <Daryl@SAGE>
2021-12-27 16:15:00 -0600

    Add project files.

---
+++ Anyfile To WAV/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Anyfile_To_WAV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(10, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(79, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(95, 25);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(74, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(175, 25);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(59, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sample Rate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Channels";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(175, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bits";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(240, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 59);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Anyfile to WAV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Anyfile To WAV/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Anyfile_To_WAV
+{
+    public partial class Form1 : Form
+    {
+        int MB = 1024 * 1024;
+        readonly string dpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        int rate = 0;
+        short chan = 0;
+        short bits = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string[] ratea = new string[3];
+            ratea[0] = "44100";
+            ratea[1] = "22050";
+            ratea[2] = "11025";
+            comboBox1.DataSource = ratea;
+            string[] chana = new string[2];
+            chana[0] = "Mono";
+            chana[1] = "Stereo";
+            comboBox2.DataSource = chana;
+            comboBox2.SelectedIndex = 1;
+            string[] bitsa = new string[2];
+            bitsa[0] = "8-bit";
+            bitsa[1] = "16-bit";
+            comboBox3.DataSource = bitsa;
+            comboBox3.SelectedIndex = 1;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "Anyfile to Wav";
+            // open
+            if (comboBox1.SelectedIndex == 0) rate = 44100;
+            if (comboBox1.SelectedIndex == 1) rate = 22050;
+            if (comboBox1.SelectedIndex == 2) rate = 11050;
+            if (comboBox2.SelectedIndex == 0) chan = 1;
+            else chan = 2;
+            if (comboBox3.SelectedIndex == 0) bits = 8;
+            else bits = 16;
+            short b = Convert.ToInt16(bits);
+            short c = Convert.ToInt16(chan);
+            short d = Convert.ToInt16(8);
+            openFileDialog1.ShowDialog();
+            string sfile = openFileDialog1.FileName;
+            string filename = Path.GetFileNameWithoutExtension(sfile) + ".wav";
+            if (File.Exists(sfile))
+            {
+                //      Check if file is too large        
+                long size = new System.IO.FileInfo(sfile).Length;
+                if (size < 4294967252)
+                {
+                    //      Start Conversion
+                    this.Text = "Converting to WAV...";
+                    //      Construct WAV file header 
+                    byte[] riff = Encoding.ASCII.GetBytes("RIFF");      // 1-4
+                    uint len = Convert.ToUInt32(size) + 44;             // 5-8
+                    byte[] wave = Encoding.ASCII.GetBytes("WAVEfmt ");  // 9-16
+                    uint st1 = 16;                                      // 17-20
+                    short st2 = 1;                                      // 21-22 -  23-24 (assigned to short 'c' from int 'chan') - 25-28 (assigned to 'rate')
+                    int sr = (rate * bits * chan) / 8;                  // 29-32 (sample rate * Bits * channels) /8
+                    short sr2 = (short)(b * c / d);                     // 33-34 (bits * channels) /8  35-36 (assigned to short 'b' from int 'bits')
+                    byte[] dat = Encoding.ASCII.GetBytes("data");       // 37-40
+                    uint dlen = Convert.ToUInt32(size);                 // 41-44
+                    //      Build WAV file
+                    var buffer = new MemoryStream();                                                // Configure variable as memory buffer
+                    var write = new BinaryWriter(buffer);                                           // Configure variable for binary writer
+                    write.Write(riff); write.Write(len); write.Write(wave);                         // Write header to memory buffer
+                    write.Write(st1); write.Write(st2); write.Write(c);                             //    
+                    write.Write(rate); write.Write(sr); write.Write(sr2);                           //
+                    write.Write(b); write.Write(dat); write.Write(dlen); write.Close();             //
+                    byte[] nfile = buffer.ToArray();                                                // Write memory buffer to byte array
+                    File.WriteAllBytes(dpath + @"\" + filename, nfile);                             // Write WAV file header
+                    FileStream Stream = new(sfile, FileMode.Open, FileAccess.Read);                 // Open source file for read only
+                    FileStream Dest = new(dpath + @"\" + filename, FileMode.Append);                // Open Destination file for append
+                    //      Construct file in 1mb blocks until completed
+                    uint W = (uint)(dlen / MB);
+                    try
+                    {
+                        for (uint i = 0; i <= W; i++)
+                        {
+                            if (i == W) { MB = (int)(dlen - (W * MB)); }
+                            byte[] buff = new byte[MB];
+                            Stream.Seek(i * MB, SeekOrigin.Begin);
+                            Stream.Read(buff, 0, MB);
+                            Dest.Write(buff, 0, buff.Length);
+                        }
+                        Stream.Close();
+                        Dest.Close();
+                    }
+                    catch { }
+                }
+                else this.Text = "File exceeds 4gb limit!";
+            }
+        }
+    }
+}
+
