8bd44900e85d06d2b3f677d7ebdc134a2dccd758
Daryl Krans <wadzinsky@hotmail.com>
2021-12-27 17:08:11 -0600

    hi?

--- Anyfile To WAV/Form1.cs
+++ Anyfile To WAV/Form1.cs
@@ -8,7 +8,7 @@ namespace Anyfile_To_WAV
     public partial class Form1 : Form
     {
         int MB = 1024 * 1024;
-        readonly string dpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        readonly string dpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // Output path for WAV file (currently the user desktop)
         int rate = 0;
         short chan = 0;
         short bits = 0;
@@ -16,6 +16,7 @@ namespace Anyfile_To_WAV
         public Form1()
         {
             InitializeComponent();
+            // Fill comboboxes with selection choices
             string[] ratea = new string[3];
             ratea[0] = "44100";
             ratea[1] = "22050";
@@ -36,7 +37,6 @@ namespace Anyfile_To_WAV
         private void Button1_Click(object sender, EventArgs e)
         {
             this.Text = "Anyfile to Wav";
-            // open
             if (comboBox1.SelectedIndex == 0) rate = 44100;
             if (comboBox1.SelectedIndex == 1) rate = 22050;
             if (comboBox1.SelectedIndex == 2) rate = 11050;
@@ -50,11 +50,11 @@ namespace Anyfile_To_WAV
             openFileDialog1.ShowDialog();
             string sfile = openFileDialog1.FileName;
             string filename = Path.GetFileNameWithoutExtension(sfile) + ".wav";
+            // Check if slected file actually exists. (not sure if this is really needed since the file is selected from the OpenFileDialog)
             if (File.Exists(sfile))
             {
-                //      Check if file is too large        
-                long size = new System.IO.FileInfo(sfile).Length;
-                if (size < 4294967252)
+                long size = new System.IO.FileInfo(sfile).Length; // sets variable 'size' to equal size of source file
+                if (size < 4294967252) // Check if file is too large -----< Planning to add support for files larger than 4gb >-  
                 {
                     //      Start Conversion
                     this.Text = "Converting to WAV...";
@@ -62,34 +62,54 @@ namespace Anyfile_To_WAV
                     byte[] riff = Encoding.ASCII.GetBytes("RIFF");      // 1-4
                     uint len = Convert.ToUInt32(size) + 44;             // 5-8
                     byte[] wave = Encoding.ASCII.GetBytes("WAVEfmt ");  // 9-16
-                    uint st1 = 16;                                      // 17-20
-                    short st2 = 1;                                      // 21-22 -  23-24 (assigned to short 'c' from int 'chan') - 25-28 (assigned to 'rate')
+                    int st1 = 16;                                       // 17-20
+                    short st2 = 1;                                      // 21-22
+                                                                        // 23-24 (assigned to short 'c' from int 'chan')
+                                                                        // 25-28 (assigned to 'rate')
                     int sr = (rate * bits * chan) / 8;                  // 29-32 (sample rate * Bits * channels) /8
-                    short sr2 = (short)(b * c / d);                     // 33-34 (bits * channels) /8  35-36 (assigned to short 'b' from int 'bits')
+                    short sr2 = (short)(b * c / d);                     // 33-34 (bits * channels) /8
+                                                                        // 35-36 (assigned to short 'b' from int 'bits')
                     byte[] dat = Encoding.ASCII.GetBytes("data");       // 37-40
                     uint dlen = Convert.ToUInt32(size);                 // 41-44
+                    
                     //      Build WAV file
-                    var buffer = new MemoryStream();                                                // Configure variable as memory buffer
-                    var write = new BinaryWriter(buffer);                                           // Configure variable for binary writer
-                    write.Write(riff); write.Write(len); write.Write(wave);                         // Write header to memory buffer
-                    write.Write(st1); write.Write(st2); write.Write(c);                             //    
-                    write.Write(rate); write.Write(sr); write.Write(sr2);                           //
-                    write.Write(b); write.Write(dat); write.Write(dlen); write.Close();             //
-                    byte[] nfile = buffer.ToArray();                                                // Write memory buffer to byte array
-                    File.WriteAllBytes(dpath + @"\" + filename, nfile);                             // Write WAV file header
-                    FileStream Stream = new(sfile, FileMode.Open, FileAccess.Read);                 // Open source file for read only
-                    FileStream Dest = new(dpath + @"\" + filename, FileMode.Append);                // Open Destination file for append
-                    //      Construct file in 1mb blocks until completed
-                    uint W = (uint)(dlen / MB);
+                    var buffer = new MemoryStream();                    // Configure variable as memory buffer
+                    var write = new BinaryWriter(buffer);               // Configure variable for binary writer
+                    
+                    // Write header to memory buffer
+                    write.Write(riff);
+                    write.Write(len);
+                    write.Write(wave);
+                    write.Write(st1);
+                    write.Write(st2);
+                    write.Write(c);
+                    write.Write(rate);
+                    write.Write(sr);
+                    write.Write(sr2);
+                    write.Write(b);
+                    write.Write(dat);
+                    write.Write(dlen);
+                    write.Close();
+                    
+                    // Write memory buffer (wave file header) to byte[] array
+                    byte[] nfile = buffer.ToArray();
+                    
+                    // Write byte array to output file (nfile)
+                    File.WriteAllBytes(dpath + @"\" + filename, nfile);               // Write WAV file header
+                    FileStream Stream = new(sfile, FileMode.Open, FileAccess.Read);   // Open source file for read only
+                    FileStream Dest = new(dpath + @"\" + filename, FileMode.Append);  // Open Destination file for append
+                    
+                    // Construct file in 1mb blocks until completed
+                    uint W = (uint)(dlen / MB);  // sets variable W to equal # of times file can be broken into 1mb sections
                     try
                     {
                         for (uint i = 0; i <= W; i++)
                         {
-                            if (i == W) { MB = (int)(dlen - (W * MB)); }
-                            byte[] buff = new byte[MB];
-                            Stream.Seek(i * MB, SeekOrigin.Begin);
-                            Stream.Read(buff, 0, MB);
-                            Dest.Write(buff, 0, buff.Length);
+                            if (i == W) { MB = (int)(dlen - (W * MB)); } // sets variable 'MB' to equal the remainder of the source file when/if < 1MB
+                            byte[] buff = new byte[MB];                  // sets variable 'buff' array size to the value of 'MB'
+                            Stream.Seek(i * MB, SeekOrigin.Begin);       // seeks file location in source file
+                            Stream.Read(buff, 0, MB);                    // reads data into variable 'buff'
+                            Dest.Write(buff, 0, buff.Length);            // writes data from 'buff' to the end of the destionation file (appends) 
                         }
                         Stream.Close();
                         Dest.Close();
