e5f9b0235b87546ff6eeb52c0121e20b0665d599
Daryl Krans <wadzinsky@hotmail.com>
2021-12-27 20:05:51 -0600

    Added more sample rates simplified the way sample rate is used in the program

--- Anyfile To WAV/Form1.cs
+++ Anyfile To WAV/Form1.cs
@@ -14,7 +14,6 @@ namespace Anyfile_To_WAV
     {
         const int MB = 1024 * 1024;
         readonly string dpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // Output path for WAV file (currently the user desktop)
-        int rate = 0;
         short chan = 0;
         short bits = 0;
         
@@ -22,35 +21,32 @@ namespace Anyfile_To_WAV
         {
             InitializeComponent();
             // Fill comboboxes with selection choices
-            string[] ratea = new string[3];
-            ratea[0] = "44100";
-            ratea[1] = "22050";
-            ratea[2] = "11025";
-            comboBox1.DataSource = ratea;
-            string[] chana = new string[2];
-            chana[0] = "Mono";
-            chana[1] = "Stereo";
-            comboBox2.DataSource = chana;
+            string[] srate = new string[10];
+            srate[0] = "8000";
+            srate[9] = "96000";
+            for (int i = 1; i < srate.Length - 1; i++)
+            { srate[i] = (i * 11025).ToString(); }
+            comboBox1.DataSource = srate;
+            comboBox1.SelectedIndex = 4;
+            string[] chnl = new string[2];
+            chnl[0] = "Mono";
+            chnl[1] = "Stereo";
+            comboBox2.DataSource = chnl;
             comboBox2.SelectedIndex = 1;
-            string[] bitsa = new string[2];
-            bitsa[0] = "8-bit";
-            bitsa[1] = "16-bit";
-            comboBox3.DataSource = bitsa;
+            string[] bts = new string[2];
+            bts[0] = "8-bit";
+            bts[1] = "16-bit";
+            comboBox3.DataSource = bts;
             comboBox3.SelectedIndex = 1;
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
             this.Text = "Anyfile to Wav";
-            if (comboBox1.SelectedIndex == 0) rate = 44100;
-            if (comboBox1.SelectedIndex == 1) rate = 22050;
-            if (comboBox1.SelectedIndex == 2) rate = 11050;
             if (comboBox2.SelectedIndex == 0) chan = 1;
             else chan = 2;
             if (comboBox3.SelectedIndex == 0) bits = 8;
             else bits = 16;
-            short b = (short)(bits);
-            short c = (short)(chan);
             short d = 8;
             openFileDialog1.ShowDialog();
             string sfile = openFileDialog1.FileName;
@@ -64,22 +60,22 @@ namespace Anyfile_To_WAV
                     //      Start Conversion
                     this.Text = "Converting to WAV...";
                     //      Construct WAV file header 
-                    byte[] riff = Encoding.ASCII.GetBytes("RIFF");      // 1-4   ASCII data 'RIFF'
-                    uint len = (uint)(size) + 44;                       // 5-8   (uint) value equals size of entire file (data + header)
-                    byte[] wave = Encoding.ASCII.GetBytes("WAVEfmt ");  // 9-16  ASCII data 'WAVEfmt ' <-- with null space
-                    int st1 = 16;                                       // 17-20 (int) Length of format data 
-                    short st2 = 1;                                      // 21-22 (short) Type of data. 1= PCM, 2= byte integer
-                                                                        // 23-24 (short) Number of channels (assigned to short 'c' from int 'chan')
-                                                                        // 25-28 (int) sample rate (assigned to 'rate')
-                    int sr = (rate * bits * chan) / 8;                  // 29-32 (int) (sample rate * Bits * channels) /8
-                    short sr2 = (short)(b * c / d);                     // 33-34 (short) (bits * channels) /8
-                                                                        // 35-36 (short) (assigned to short 'b' from int 'bits')
-                    byte[] dat = Encoding.ASCII.GetBytes("data");       // 37-40 ASCII data 'data'
-                    uint dlen = (uint)(size);                           // 41-44 (uint) size of data chunk (the actual wave data) 
+                    byte[] riff = Encoding.ASCII.GetBytes("RIFF");           // 1-4   ASCII data 'RIFF'
+                    uint len = (uint)(size) + 44;                            // 5-8   (uint) value equals size of entire file (data + header)
+                    byte[] wave = Encoding.ASCII.GetBytes("WAVEfmt ");       // 9-16  ASCII data 'WAVEfmt ' <-- with null space
+                    int st1 = 16;                                            // 17-20 (int) Length of format data 
+                    short st2 = 1;                                           // 21-22 (short) Type of data. 1= PCM, 2= byte integer
+                    short c = (short)(chan);                                 // 23-24 (short) Number of channels
+                    int rate = int.Parse(comboBox1.SelectedItem.ToString()); // 25-28 (int) sample rate
+                    int sr = (rate * bits * chan) / 8;                       // 29-32 (int) (sample rate * Bits * channels) /8
+                    short sr2 = (short)(bits * c / d);                       // 33-34 (short) (bits * channels) /8
+                    short b = (short)(bits);                                 // 35-36 (short) bits per channel
+                    byte[] dat = Encoding.ASCII.GetBytes("data");            // 37-40 ASCII data 'data'
+                    uint dlen = (uint)(size);                                // 41-44 (uint) size of data chunk (the actual wave data) 
                     
                     //      Build WAV file
-                    var buffer = new MemoryStream();                    // Configure variable as memory buffer
-                    var write = new BinaryWriter(buffer);               // Configure variable for binary writer
+                    var buffer = new MemoryStream();                         // Configure variable as memory buffer
+                    var write = new BinaryWriter(buffer);                    // Configure variable for binary writer
                     
                     // Write header to memory buffer
                     write.Write(riff);
@@ -109,14 +105,14 @@ namespace Anyfile_To_WAV
                     {
                         label5.Visible = true;
                         int length = MB;
-                        uint W = (uint)(dlen / MB);                      // sets variable W to equal # of times file can be broken into 1mb sections
-                        for (uint i = 0; i <= W; i++)                    // for loop to process file read/write from start to finish
+                        uint W = (uint)(dlen / MB);                          // sets variable W to equal # of times file can be broken into 1mb sections
+                        for (uint i = 0; i <= W; i++)                        // for loop to process file read/write from start to finish
                         {
                             if (i == W) { length = (int)(dlen - (W * MB)); } // sets variable 'MB' to equal the remainder of the source file when/if < 1MB
                             byte[] buff = new byte[length];                  // sets variable 'buff' array size to the value of 'MB'
-                            Stream.Seek(i * MB, SeekOrigin.Begin);       // seeks file location in source file
+                            Stream.Seek(i * MB, SeekOrigin.Begin);           // seeks file location in source file
                             Stream.Read(buff, 0, length);                    // reads data into variable 'buff'
-                            Dest.Write(buff, 0, buff.Length);            // writes data from 'buff' to the end of the destionation file (appends) 
+                            Dest.Write(buff, 0, buff.Length);                // writes data from 'buff' to the end of the destionation file (appends) 
                             label5.Text = "Progress : " + (i * MB).ToString("N0") + " of " + size.ToString("N0");
                             label5.Refresh();
                         }
@@ -129,11 +125,6 @@ namespace Anyfile_To_WAV
                 }
                 else this.Text = "File exceeds 4gb limit!";
             }
-        }
-
-        private void Label4_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
